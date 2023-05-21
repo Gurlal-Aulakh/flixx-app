7981fa5c92fc952f619685da2230c5ae2a47536d
Gurlal-Aulakh <gurlalaulakh246@gmail.com>
2023-05-20 21:20:22 -0400

    version2 --> Till this commit I have fetched movies from the api and have shown them on the index page.

--- js/script.js
+++ js/script.js
@@ -17,8 +17,9 @@ function init() {
 
     switch(global.currentPage){
         case '/':
-            console.log("Home");
-            break;
+            case '/index.html':
+                getPopularMovies();
+                break;
             case '/movie-details.html':
                 console.log("Movie Detail");
                 break;  
@@ -34,6 +35,55 @@ function init() {
     }
 
     changeColorOfActivePage();
+    
+}
+
+async function getPopularMovies(){
+    let {results} = await fetchDataFromAPI("movie/popular");
+    console.log(results);
+    console.log(typeof(results));
+    // result=Array.from(result);
+    // result=Object.entries(result);
+    // console.log(result);
+    // console.log(typeof(result));
+    results.forEach(movie => {
+        const div=document.createElement("div");
+        div.classList.add('card');
+        div.innerHTML=`
+        <a href="movie-details.html?id=${movie.id}">
+          ${
+            movie.poster_path?
+            `<img
+            src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+            class="card-img-top"
+            alt="${movie.title}"
+          />`:
+           `<img
+src="../images/no-image.jpg"
+class="card-img-top"
+alt="${movie.title}"
+/>`
+   }
+        </a>
+        <div class="card-body">
+          <h5 class="card-title">${movie.title}</h5>
+          <p class="card-text">
+            <small class="text-muted">Release: ${movie.release_date}</small>
+          </p>
+        </div>
+      `
+
+      document.querySelector("#popular-movies").appendChild(div);
+    });
+}
+
+async function fetchDataFromAPI(endpoint){
+    const API_KEY="65536974fc5cafe1b639977f36b87685";
+    const API_URL="https://api.themoviedb.org/3/";
+    const response=await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
+    const data =await response.json();
+    console.log(typeof(data));
+    return data;
     
 }
 
