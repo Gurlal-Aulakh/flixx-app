581b16aa0f339dd5adb4b3812643bb47cb3a62bf
Gurlal-Aulakh <gurlalaulakh246@gmail.com>
2023-05-23 16:16:38 -0400

    version 3- Till Now we have shown popular shows on tv shows page

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ function init() {
                 console.log("Movie Detail");
                 break;  
                 case '/shows.html':
-                console.log("TV shows");
+                    getPopularShows();
                 break; 
                 case '/tv-details.html':
                     console.log("TV detail");
@@ -74,6 +74,47 @@ alt="${movie.title}"
       `
 
       document.querySelector("#popular-movies").appendChild(div);
+    });
+}
+
+
+
+async function getPopularShows(){
+    let {results} = await fetchDataFromAPI("tv/popular");
+    console.log(results);
+    console.log(typeof(results));
+    // result=Array.from(result);
+    // result=Object.entries(result);
+    // console.log(result);
+    // console.log(typeof(result));
+    results.forEach(show => {
+        const div=document.createElement("div");
+        div.classList.add('card');
+        div.innerHTML=`
+        <a href="tv-details.html?id=${show.id}">
+          ${
+            show.poster_path?
+            `<img
+            src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+            class="card-img-top"
+            alt="${show.name}"
+            />`:
+           `<img
+src="../images/no-image.jpg"
+class="card-img-top"
+alt="${show.name}"
+/>`
+   }
+        </a>
+        <div class="card-body">
+          <h5 class="card-title">${show.name}</h5>
+          <p class="card-text">
+            <small class="text-muted">Air Date: ${show.first_air_date}</small>
+          </p>
+        </div>
+      `
+
+      document.querySelector("#popular-shows").appendChild(div);
     });
 }
 
